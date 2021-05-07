8796b37993132081adfa334e50e0e4eacae1956b
Jheimison Goncalves <academiafighter@hotmail.com>
2021-05-07 11:00:03 -0300

    Project created

---
+++ backend/src/test/java/com/Jheimison/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jheimison.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
