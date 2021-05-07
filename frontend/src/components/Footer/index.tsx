fd1108d90008f17a2c64e58bedf6b62b94b5e4ce
Jheimison Goncalves <academiafighter@hotmail.com>
2021-05-07 17:31:16 -0300

    Alter Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/JheimisonGoncalves" target="_blank" rel="noreferrer">Jheimison Gonlçaves</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/jheimisfran" target="_blank" rel="noreferrer">@jheimisfran</a></small></p>
             </div>
         </footer>
     );
