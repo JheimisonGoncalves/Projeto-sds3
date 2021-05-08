91f43c0afc19e678859fbfe347a1b88b63caf2b4
Jheimison Goncalves <academiafighter@hotmail.com>
2021-05-08 15:12:37 -0300

    Pagination

---
+++ backend/src/main/java/com/Jheimison/dsvendas/Services/SaleService.java
@@ -0,0 +1,29 @@
+package com.Jheimison.dsvendas.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.Jheimison.dsvendas.dto.SaleDTO;
+import com.Jheimison.dsvendas.entities.Sale;
+import com.Jheimison.dsvendas.repositories.SaleRepository;
+import com.Jheimison.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
---
+++ backend/src/main/java/com/Jheimison/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.Jheimison.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Jheimison.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long > {
+
+}
