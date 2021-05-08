c39ee0752a212df7f3e05768ae2eceb6545860a7
Jheimison Goncalves <academiafighter@hotmail.com>
2021-05-08 13:06:43 -0300

    Layers

---
+++ backend/src/main/java/com/Jheimison/dsvendas/controllers/SellerControler.java
@@ -0,0 +1,27 @@
+package com.Jheimison.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Jheimison.dsvendas.Services.SellerService;
+import com.Jheimison.dsvendas.dto.SellerDTO;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerControler {
+	
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll() {
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+}
---
+++ backend/src/main/java/com/Jheimison/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.Jheimison.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Jheimison.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long > {
+
+}
